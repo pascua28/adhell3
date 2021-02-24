7e57762cc924c607e7d9d9f9b4cc891aeffa43d8
fusionjack <dogfight60-fusionjack@yahoo.de>
2021-02-24 21:41:48 +0100

    Improve logcat logging
    Change-Id: Ic356ffc35ff428a25e8f03c580257d41d6a65fcb
    Reviewed-on: http://pi4.lan:8080/c/adhell3/+/285
    Reviewed-by: fusionjack <dogfight60-fusionjack@yahoo.de>

--- app/src/main/java/com/fusionjack/adhell3/utils/LogUtils.java
+++ app/src/main/java/com/fusionjack/adhell3/utils/LogUtils.java
@@ -5,17 +5,24 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
+import com.fusionjack.adhell3.App;
 import com.fusionjack.adhell3.BuildConfig;
 import com.samsung.android.knox.EnterpriseDeviceManager;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by fusionjack on 15/03/2018.
  */
 
 public final class LogUtils {
+
+    private static final String TAG = "adhell3";
+    private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.US);
 
     private LogUtils() {
     }
@@ -24,10 +31,11 @@ public final class LogUtils {
         info("Build version: " + BuildConfig.VERSION_NAME);
         info("Knox API: " + EnterpriseDeviceManager.getAPILevel());
         info("Android API: " + Build.VERSION.SDK_INT);
-        String filename = String.format("adhell_logcat_%s.txt", System.currentTimeMillis());
+        String filename = String.format("adhell_logcat_%s.txt", now());
         File logFile = FileUtils.toFile(filename);
         try {
-            Runtime.getRuntime().exec( "logcat -f " + logFile + " | grep com.fusionjack.adhell3");
+            String ownPackageName = App.get().getApplicationContext().getPackageName();
+            Runtime.getRuntime().exec( "logcat -f " + logFile + " | grep " + ownPackageName);
         } catch (IOException e) {
             error(e.getMessage(), e);
             return "";
@@ -36,7 +44,7 @@ public final class LogUtils {
     }
 
     public static void info(String text) {
-        Log.i(getCallerInfo(), text);
+        Log.i(TAG, buildMessage(text));
     }
 
     public static void info(String text, Handler handler) {
@@ -44,15 +52,15 @@ public final class LogUtils {
             Message message = handler.obtainMessage(0, text);
             message.sendToTarget();
         }
-        Log.i(getCallerInfo(), text);
+        Log.i(TAG, buildMessage(text));
     }
 
     public static void error(String text) {
-        Log.e(getCallerInfo(), text);
+        Log.e(TAG, buildMessage(text));
     }
 
     public static void error(String text, Throwable e) {
-        Log.e(getCallerInfo(), text, e);
+        Log.e(TAG, buildMessage(text), e);
     }
 
     public static void error(String text, Throwable e, Handler handler) {
@@ -60,14 +68,24 @@ public final class LogUtils {
             Message message = handler.obtainMessage(0, text);
             message.sendToTarget();
         }
-        Log.e(getCallerInfo(), text, e);
+        Log.e(TAG, buildMessage(text), e);
+    }
+    
+    private static String now() {
+        return FORMATTER.format(new Date());
+    }
+
+    private static String buildMessage(String text) {
+        return getCallerInfo() + " " + text;
     }
 
     private static String getCallerInfo() {
         StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
-        if (stackTraceElements.length > 4) {
-            return stackTraceElements[4].getClassName() + "(" + stackTraceElements[4].getMethodName() + ")";
+        if (stackTraceElements.length > 5) {
+            StackTraceElement element = stackTraceElements[5];
+            return element.getClassName() + "(" + element.getMethodName() + ")";
         }
         return "Empty class name";
     }
+    
 }
