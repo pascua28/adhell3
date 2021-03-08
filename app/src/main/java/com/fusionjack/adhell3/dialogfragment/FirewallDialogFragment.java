96ecb972b738e2650d89eeebb2a045a798e95b5b
fusionjack <dogfight60-fusionjack@yahoo.de>
2021-03-08 09:58:39 +0100

    Style domain and firewall dialog
    Change-Id: I9d22cd5a500f2421d4aef8a65532251e49e8afcc
    Reviewed-on: http://pi4.lan:8080/c/adhell3/+/308
    Reviewed-by: fusionjack <dogfight60-fusionjack@yahoo.de>

--- app/src/main/java/com/fusionjack/adhell3/dialogfragment/FirewallDialogFragment.java
+++ app/src/main/java/com/fusionjack/adhell3/dialogfragment/FirewallDialogFragment.java
@@ -1,20 +1,25 @@
 package com.fusionjack.adhell3.dialogfragment;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+
 import com.fusionjack.adhell3.R;
 
 public class FirewallDialogFragment extends DialogFragment {
-    private TextView titleTextView;
     private TextView logTextView;
     private ScrollView scrollView;
     private Button closeButton;
@@ -29,28 +34,33 @@ public class FirewallDialogFragment extends DialogFragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.DialogStyle);
+    }
 
-        Dialog dialog = getDialog();
-        if (dialog != null) {
-            int width = (int)(getResources().getDisplayMetrics().widthPixels * 0.9);
-            int height = (int)(getResources().getDisplayMetrics().heightPixels * 0.9);
-            dialog.getWindow().setLayout(width, height);
-        }
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+
+        int width = (int)(getResources().getDisplayMetrics().widthPixels * 0.9);
+        int height = (int)(getResources().getDisplayMetrics().heightPixels * 0.9);
+        dialog.getWindow().setLayout(width, height);
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        dialog.getWindow().setDimAmount(0.75f);
+
+        return dialog;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dialog_fragment_firewall, container);
-    }
+        View view = inflater.inflate(R.layout.dialog_fragment_firewall, container);
 
-    @Override
-    public void onViewCreated(View view, @Nullable final Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        titleTextView = view.findViewById(R.id.titleTextView);
+        TextView titleTextView = view.findViewById(R.id.titleTextView);
         Bundle bundle = getArguments();
         if (bundle != null) {
             titleTextView.setText(bundle.getString("title"));
@@ -59,13 +69,13 @@ public class FirewallDialogFragment extends DialogFragment {
         scrollView  = view.findViewById(R.id.scrollView);
         logTextView = view.findViewById(R.id.logTextView);
         closeButton = view.findViewById(R.id.closeButton);
-        closeButton.setOnClickListener(v -> {
-            dismiss();
-        });
+        closeButton.setOnClickListener(v -> dismiss());
         closeButton.setEnabled(false);
+        closeButton.setTextColor(Color.GRAY);
 
-        int height = (int)(getResources().getDisplayMetrics().heightPixels * 0.675);
-        scrollView.getLayoutParams().height = height;
+        scrollView.getLayoutParams().height = (int)(getResources().getDisplayMetrics().heightPixels * 0.7);
+
+        return view;
     }
 
     public void appendText(String text) {
@@ -80,5 +90,6 @@ public class FirewallDialogFragment extends DialogFragment {
 
     public void enableCloseButton() {
         closeButton.setEnabled(true);
+        closeButton.setTextColor(ContextCompat.getColor(getContext(), R.color.colorAccent));
     }
 }
